ea968e4194c91b8780cbf7e69983f2bbb00bb647
Nan Ding <nding@LT-MAC-1.local>
2019-01-29 13:51:54 -0800

    design a sender able to send html

---
+++ EmailSending/html_sender.py
@@ -0,0 +1,46 @@
+import smtplib, ssl, getpass
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+sender_email = "ericftp2019@gmail.com"
+receiver_email = "ndingdw@gmail.com"
+password = getpass.getpass(prompt="Type you password and press enter: ")
+
+message = MIMEMultipart("alternative")
+message["Subject"] = "multpart test"
+message["From"] = sender_email
+message["To"] = receiver_email
+
+#Creaet the plain-text and HTML version of your message
+
+text = """\
+Hi,
+How are you?
+Real Python has many great tutorials:
+www.realpython.com"""
+
+html = """\
+<html>
+  <body>
+    <p>Hi,<br>
+       How are you?<br>
+       <a href="http://www.realpython.com">Real Python</a> 
+       has many great tutorials.
+    </p>
+  </body>
+</html>
+"""
+# Turn these into plain/html MIMEText objects
+part1 = MIMEText(text, "plain")
+part2 = MIMEText(html, "html")
+
+# Add HTML/plain-text parts to MIMEMultipart message and the emial clinet will try to send the last part first
+message.attach(part1)
+message.attach(part2)
+
+# create secure connection with server and send email
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+	server.login(sender_email, password)
+	server.sendmail(sender_email, receiver_email, message.as_string())
